14b2f3874d1f6009ec69bba8ffc55fd4ee064d5e
Nguyen Khac Khanh <encacap@gmail.com>
2022-04-12 11:31:24 +0700

    220412_1131

--- webpack-demo.config.js
+++ webpack-demo.config.js
@@ -57,7 +57,7 @@ module.exports = {
                     {
                         loader: "url-loader",
                         options: {
-                            limit: 8192
+                            limit: true
                         }
                     }
                 ]
