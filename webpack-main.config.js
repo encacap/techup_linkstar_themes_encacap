8cacedfac6440c503657a75c1643bb8abe5164e0
Nguyen Khac Khanh <encacap@gmail.com>
2022-04-12 11:22:20 +0700

    220412_1122

--- webpack-main.config.js
+++ webpack-main.config.js
@@ -72,7 +72,10 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
-                        loader: "file-loader"
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192
+                        }
                     }
                 ]
             }
